abbef99225bb2ca294eae300fe7be15cf295e1e2
Robin Lebert <robin.lebert@webtales.fr>
2014-09-12 13:07:37 +0200

    languageMenu block done

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -825,19 +825,27 @@
             var me = this;
             var config = $scope.blockConfig;
             me.languages = $scope.rubedo.current.site.languages;
-            me.currentLang = $route.current.params.lang;
-            me.changeLang = function (lang) {
-                RubedoModuleConfigService.changeLang(lang);
-                RubedoPagesService.getPageById($scope.rubedo.current.page.id).then(function(response){
-                    if (response.data.success){
-                        console.log(response.data);
-                        $route.current.params.route =  response.data.url;
-                        $location.path(response.data.url);
-                    }
-                });
+            me.currentLang = $scope.rubedo.current.site.languages[$route.current.params.lang];
+            me.mode = config.displayAs == "select";
+            me.isDisabled =  function(lang){
+                return me.currentLang.lang == lang;
+            }
+            if(!config.showCurrentLanguage){
+                delete me.languages[$route.current.params.lang];
+            }
+            me.getFlagUrl = function(flagCode){
+                return '/assets/flags/16/'+flagCode+'.png';
             };
-            console.log($scope.rubedo.current.page.id);
-            console.log($location.path());
+            me.changeLang = function (lang) {
+                if(lang != me.currentLang.lang){
+                    RubedoModuleConfigService.changeLang(lang);
+                    RubedoPagesService.getPageById($scope.rubedo.current.page.id).then(function(response){
+                        if (response.data.success){
+                            $location.path(response.data.url);
+                        }
+                    });
+                }
+            };
         }]);
 
 })();
