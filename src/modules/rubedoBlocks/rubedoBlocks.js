d1c617a7b4b638f85998a976aa092c10d4128c00
Robin Lebert <robin.lebert@webtales.fr>
2014-09-17 12:42:20 +0200

    Block AddThisFollow Done

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -33,7 +33,8 @@
         twitter:"/components/webtales/rubedo-frontoffice/templates/blocks/twitter.html",
         geoSearchResults:"/components/webtales/rubedo-frontoffice/templates/blocks/geoSearchResults.html",
         addThis:"/components/webtales/rubedo-frontoffice/templates/blocks/addThisShare.html",
-        resource:"/components/webtales/rubedo-frontoffice/templates/blocks/mediaDownload.html"
+        resource:"/components/webtales/rubedo-frontoffice/templates/blocks/mediaDownload.html",
+        addThisFollow:"/components/webtales/rubedo-frontoffice/templates/blocks/addThisFollow.html"
     };
 
     var responsiveClasses = {
@@ -1500,6 +1501,34 @@
                 me.media =  response.data.media;
             }
         });
+    }]);
+
+    module.controller('AddThisFollowController',['$scope',function($scope){
+        var me = this;
+        var config = $scope.blockConfig;
+        me.networks = [];
+        me.divClass = 'addthis_toolbox';
+        if(config.small == 1){
+            me.divClass += ' addthis_32x32_style';
+        }
+        if(config.disposition == 'Horizontal'){
+            me.divClass += ' addthis_default_style';
+        } else {
+            me.divClass += ' addthis_vertical_style';
+        }
+        me.networkClass = function(network){
+          return 'addthis_button_'+network.name+'_follow';
+        };
+        angular.forEach(config, function(value,key){
+            if(key != 'disposition' && key != 'small'){
+                var network = {
+                    name: key,
+                    id: value
+                };
+                me.networks.push(network);
+            }
+        });
+        addthis.toolbox('.addthis_toolbox');
     }]);
 
 })();
