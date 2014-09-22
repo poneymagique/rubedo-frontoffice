052a4cb11623039dd2a85021ab1155cadd80ca64
Robin Lebert <robin.lebert@webtales.fr>
2014-09-22 14:23:27 +0200

    Block gallery done

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -1676,12 +1676,16 @@
     module.controller('GalleryController',['$scope','RubedoMediaService',function($scope,RubedoMediaService){
         var me = this;
         var config = $scope.blockConfig;
-        var prefix = 'gallery_'+$scope.block.id;
-        console.log(config);
+        me.start = 0;
+        me.limit = config.pageSize?config.pageSize:8;
+        me.changePageAction = function(){
+            options.start = me.start;
+            me.getMedia(options);
+        };
         var options = {
             query: config.query,
-            start: 0,
-            limit: 8,
+            start: me.start,
+            limit: me.limit,
             pageWorkspace: $scope.rubedo.current.page.workspace,
             imageThumbnailHeight: config.imageThumbnailHeight?config.imageThumbnailHeight:100,
             imageThumbnailWidth: config.imageThumbnailWidth?config.imageThumbnailWidth:100
@@ -1690,9 +1694,24 @@
         me.getMedia = function(options){
             RubedoMediaService.getMediaByQuery(options).then(function(response){
                 if(response.data.success){
+                    me.count = response.data.count;
                     me.images = response.data.media.data;
+                    console.log(me.images);
                 }
             });
+        };
+        me.loadModal = function(index){
+            angular.element('#rubedoGalleryDetailModal').appendTo('body').modal('show');
+            me.currentIndex = index;
+            me.currentImage = me.images[me.currentIndex];
+        };
+        me.changeImage = function(side){
+            if(side == 'left' && me.currentIndex > 0){
+                me.currentIndex -= 1;
+            } else if(side == 'right' && me.currentIndex < me.images.length - 1) {
+                me.currentIndex += 1;
+            }
+            me.currentImage = me.images[me.currentIndex];
         };
         me.getMedia(options);
     }]);
