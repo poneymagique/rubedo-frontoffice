a616ed801b4672c212dba553891e7565650d9472
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-09-23 12:50:19 +0200

    switch to centralized notif system

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -1637,7 +1637,7 @@
         addthis.toolbox('.addthis_toolbox');
     }]);
 
-    module.controller('SignUpController',['$scope','RubedoUserTypesService','RubedoUsersService', function($scope, RubedoUserTypesService, RubedoUsersService){
+    module.controller('SignUpController',['$scope','RubedoUserTypesService','RubedoUsersService', '$location', function($scope, RubedoUserTypesService, RubedoUsersService, $location){
         var me = this;
         var config = $scope.blockConfig;
         me.inputFields=[ ];
@@ -1680,6 +1680,8 @@
 
 
         };
+        var queryParams=$location.search();
+        console.log(queryParams);
         if (config.userType){
             RubedoUserTypesService.getUserTypeById(config.userType).then(
                 function(response){
