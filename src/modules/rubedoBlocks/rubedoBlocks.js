126acdaaf6671c1717c664eccd5fcf615edcb5bb
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-09-24 11:34:55 +0200

    fix image block

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -1897,4 +1897,20 @@
             }
         };
     }]);
+
+    module.controller("ImageController",["$scope","RubedoPagesService", function($scope,RubedoPagesService){
+        var me = this;
+        var config = $scope.blockConfig;
+        if (config.externalURL){
+            me.url=config.externalURL;
+        } else if (config.imageLink&&mongoIdRegex.test(config.imageLink)){
+            RubedoPagesService.getPageById(config.imageLink).then(function(response){
+                if (response.data.success){
+                    me.url=response.data.url;
+                }
+            });
+        }
+
+    }]);
+
 })();
