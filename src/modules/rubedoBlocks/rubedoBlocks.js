6b08a980eaa5a6f1bfbe8c7d2af1ebddadbd8ed5
Robin Lebert <robin.lebert@webtales.fr>
2014-09-26 17:14:30 +0200

    no api request if siteMap block parameters are empty

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -1188,32 +1188,34 @@
 
     module.controller("SiteMapController",['$scope','$location','RubedoMenuService',function($scope,$location,RubedoMenuService){
         var me=this;
-        me.menu={};
-        me.currentRouteline=$location.path();
         var config=$scope.blockConfig;
-        var pageId=$scope.rubedo.current.page.id;
-        me.hidePages = true;
-        me.hideChildPages = true;
-        me.showPagesClick = function(pageType){
-            if(pageType == 'pages'){
-                me.hidePages = !me.hidePages;
-            } else {
-                me.hideChildPages = !me.hideChildPages;
+        if(config.rootPage){
+            me.menu={};
+            me.currentRouteline=$location.path();
+            var pageId=$scope.rubedo.current.page.id;
+            me.hidePages = true;
+            me.hideChildPages = true;
+            me.showPagesClick = function(pageType){
+                if(pageType == 'pages'){
+                    me.hidePages = !me.hidePages;
+                } else {
+                    me.hideChildPages = !me.hideChildPages;
+                }
+            };
+            if(config.displayLevel == 2){
+                me.hidePages = false;
+            } else if (config.displayLevel > 2){
+                me.hidePages = false;
+                me.hideChildPages = false;
             }
-        };
-        if(config.displayLevel == 2){
-            me.hidePages = false;
-        } else if (config.displayLevel > 2){
-            me.hidePages = false;
-            me.hideChildPages = false;
+            RubedoMenuService.getMenu(config.rootPage, 5).then(function(response){
+                if (response.data.success){
+                    me.menu=response.data.menu;
+                } else {
+                    me.menu={};
+                }
+            });
         }
-        RubedoMenuService.getMenu(config.rootPage, 5).then(function(response){
-            if (response.data.success){
-                me.menu=response.data.menu;
-            } else {
-                me.menu={};
-            }
-        });
     }]);
 
     module.controller('TwitterController',['$scope',function($scope){
