9f818b7ea7a13f5c0235a059c9b16418454ea0b6
Robin Lebert <robin.lebert@webtales.fr>
2014-09-26 17:09:36 +0200

    no api request if imageMap block parameters are empty

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -226,7 +226,9 @@
                 me.getContents(config.query, pageId, siteId, options, true);
             }
         };
-        me.getContents(config.query, pageId, siteId, options, false);
+        if(config.query){
+            me.getContents(config.query, pageId, siteId, options, false);
+        }
     }]);
 
     module.directive("paginator",["$timeout",function($timeout){
@@ -1832,30 +1834,35 @@
     module.controller('ImageMapController',['$scope','$element','RubedoMediaService',function($scope,$element,RubedoMediaService){
         var me = this;
         var config = $scope.blockConfig;
-        var map = angular.fromJson(config.map);
-        RubedoMediaService.getMediaById(config.image).then(function(response){
-            if(response.data.success){
-                me.image =  response.data.media;
-            }
-        });
-        me.prefix = config.image+'map';
-        angular.forEach(map, function(mapElement, key){
-            if(mapElement.type == 'polygon'){
-                map[key]['type'] = 'poly';
-            }
-            if(mapElement.type == 'rect'){
-                map[key]['params']['x1'] = mapElement.params.x + mapElement.params.width;
-                map[key]['params']['y1'] = mapElement.params.y + mapElement.params.height;
-                map[key]['coords'] = mapElement.params.x+','+mapElement.params.y+','+map[key]['params']['x1']+','+map[key]['params']['y1'];
-            } else {
-                map[key]['coords'] = '';
-                angular.forEach(mapElement.params,function(value){
-                    map[key]['coords'] += value+',';
-                });
-            }
-        });
-        me.map = map;
-        $element.find('img[usemap]').rwdImageMaps();
+        if(config.image){
+            RubedoMediaService.getMediaById(config.image).then(function(response){
+                if(response.data.success){
+                    me.image =  response.data.media;
+                }
+            });
+            me.prefix = config.image+'map';
+        }
+        if(config.map){
+            var map = angular.fromJson(config.map);
+            angular.forEach(map, function(mapElement, key){
+                if(mapElement.type == 'polygon'){
+                    map[key]['type'] = 'poly';
+                }
+                if(mapElement.type == 'rect'){
+                    map[key]['params']['x1'] = mapElement.params.x + mapElement.params.width;
+                    map[key]['params']['y1'] = mapElement.params.y + mapElement.params.height;
+                    map[key]['coords'] = mapElement.params.x+','+mapElement.params.y+','+map[key]['params']['x1']+','+map[key]['params']['y1'];
+                } else {
+                    map[key]['coords'] = '';
+                    angular.forEach(mapElement.params,function(value){
+                        map[key]['coords'] += value+',';
+                    });
+                }
+            });
+            me.map = map;
+            $element.find('img[usemap]').rwdImageMaps();
+        }
+
     }]);
 
     module.controller('ContactController',['$scope','RubedoContactService',function($scope,RubedoContactService){
