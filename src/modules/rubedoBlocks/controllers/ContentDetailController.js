91703a678a6b7b75c827322c358b30544799f4ba
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-10-07 13:30:18 +0200

    externalize content detail

---
+++ src/modules/rubedoBlocks/controllers/ContentDetailController.js
@@ -0,0 +1,90 @@
+angular.module("rubedoBlocks").lazy.controller("ContentDetailController",["$scope","RubedoContentsService",function($scope, RubedoContentsService){
+    var me = this;
+    var config = $scope.blockConfig;
+    var themePath="/theme/"+window.rubedoConfig.siteTheme;
+    $scope.fieldInputMode=false;
+    $scope.$watch('rubedo.fieldEditMode', function(newValue) {
+        $scope.fieldEditMode=me.content&&me.content.readOnly ? false : newValue;
+
+    });
+    me.getFieldByName=function(name){
+        var field=null;
+        angular.forEach(me.content.type.fields,function(candidate){
+            if (candidate.config.name==name){
+                field=candidate;
+            }
+        });
+        return field;
+    };
+
+    me.getContentById = function (contentId){
+        var options = {
+            siteId: $scope.rubedo.current.site.id,
+            pageId: $scope.rubedo.current.page.id
+        };
+        RubedoContentsService.getContentById(contentId, options).then(
+            function(response){
+                if(response.data.success){
+                    $scope.rubedo.current.page.contentCanonicalUrl = response.data.content.canonicalUrl;
+                    me.content=response.data.content;
+                    $scope.fieldEntity=angular.copy(me.content.fields);
+                    $scope.fieldLanguage=me.content.locale;
+                    me.content.type.fields.unshift({
+                        cType:"title",
+                        config:{
+                            name:"text",
+                            fieldLabel:"Title",
+                            allowBlank:false
+                        }
+                    });
+                    $scope.rubedo.current.breadcrumb.push({title:response.data.content.text});
+                    if (me.content.type.activateDisqus&&$scope.rubedo.current.site.disqusKey){
+                        me.activateDisqus=true;
+                        me.disqusShortname=$scope.rubedo.current.site.disqusKey;
+                        me.disqusIdentifier=me.content.id;
+                        me.disqusUrl=window.location.href;
+                        me.disqusTitle=me.content.text;
+                    }
+                    me.customLayout=null;
+                    if (angular.isArray(me.content.type.layouts)){
+                        angular.forEach(me.content.type.layouts,function(layout){
+                            if (layout.active&&layout.site==$scope.rubedo.current.site.id){
+                                me.customLayout=layout;
+                            }
+                        });
+                    }
+                    if (me.customLayout&&me.customLayout.customTemplate){
+                        me.detailTemplate=themePath+'/templates/blocks/contentDetail/customTemplate.html';
+                    } else if (me.customLayout){
+                        me.detailTemplate=themePath+'/templates/blocks/contentDetail/customLayout.html';
+                    } else {
+                        me.detailTemplate=themePath+'/templates/blocks/contentDetail/default.html';
+                    }
+                }
+            }
+        );
+    };
+    if (config.contentId){
+        me.getContentById(config.contentId);
+    }
+    me.revertChanges=function(){
+        $scope.fieldEntity=angular.copy(me.content.fields);
+    };
+    me.registerEditChanges=function(){
+        $scope.rubedo.registerEditCtrl(me);
+    };
+    me.persistChanges=function(){
+        var payload=angular.copy(me.content);
+        payload.fields=angular.copy($scope.fieldEntity);
+        delete (payload.type);
+        RubedoContentsService.updateContent(payload).then(
+            function(response){
+                $scope.rubedo.addNotification("success","Content updated.");
+            },
+            function(response){
+                $scope.rubedo.addNotification("error","Content update error.");
+            }
+        );
+    };
+    $scope.registerFieldEditChanges=me.registerEditChanges;
+}]);
--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -47,7 +47,8 @@
             "internalDependencies":["/src/modules/rubedoBlocks/controllers/RichTextController.js"]
         },
         "contentDetail": {
-            "template": "/templates/blocks/contentDetail.html"
+            "template": "/templates/blocks/contentDetail.html",
+            "internalDependencies":["/src/modules/rubedoBlocks/controllers/ContentDetailController.js"]
         },
         "calendar": {
             "template": "/templates/blocks/calendar.html"
@@ -425,95 +426,7 @@
         }
     }]);
 
-    module.controller("ContentDetailController",["$scope","RubedoContentsService",function($scope, RubedoContentsService){
-        var me = this;
-        var config = $scope.blockConfig;
-        $scope.fieldInputMode=false;
-        $scope.$watch('rubedo.fieldEditMode', function(newValue) {
-            $scope.fieldEditMode=me.content&&me.content.readOnly ? false : newValue;
 
-        });
-        me.getFieldByName=function(name){
-            var field=null;
-            angular.forEach(me.content.type.fields,function(candidate){
-                if (candidate.config.name==name){
-                    field=candidate;
-                }
-            });
-            return field;
-        };
-
-        me.getContentById = function (contentId){
-            var options = {
-                siteId: $scope.rubedo.current.site.id,
-                pageId: $scope.rubedo.current.page.id
-            };
-            RubedoContentsService.getContentById(contentId, options).then(
-                function(response){
-                    if(response.data.success){
-                        $scope.rubedo.current.page.contentCanonicalUrl = response.data.content.canonicalUrl;
-                        me.content=response.data.content;
-                        $scope.fieldEntity=angular.copy(me.content.fields);
-                        $scope.fieldLanguage=me.content.locale;
-                        me.content.type.fields.unshift({
-                            cType:"title",
-                            config:{
-                                name:"text",
-                                fieldLabel:"Title",
-                                allowBlank:false
-                            }
-                        });
-                        $scope.rubedo.current.breadcrumb.push({title:response.data.content.text});
-                        if (me.content.type.activateDisqus&&$scope.rubedo.current.site.disqusKey){
-                            me.activateDisqus=true;
-                            me.disqusShortname=$scope.rubedo.current.site.disqusKey;
-                            me.disqusIdentifier=me.content.id;
-                            me.disqusUrl=window.location.href;
-                            me.disqusTitle=me.content.text;
-                        }
-                        me.customLayout=null;
-                        if (angular.isArray(me.content.type.layouts)){
-                            angular.forEach(me.content.type.layouts,function(layout){
-                                if (layout.active&&layout.site==$scope.rubedo.current.site.id){
-                                    me.customLayout=layout;
-                                }
-                            });
-                        }
-                        if (me.customLayout&&me.customLayout.customTemplate){
-                            me.detailTemplate=themePath+'/templates/blocks/contentDetail/customTemplate.html';
-                        } else if (me.customLayout){
-                            me.detailTemplate=themePath+'/templates/blocks/contentDetail/customLayout.html';
-                        } else {
-                            me.detailTemplate=themePath+'/templates/blocks/contentDetail/default.html';
-                        }
-                    }
-                }
-            );
-        };
-        if (config.contentId){
-            me.getContentById(config.contentId);
-        }
-        me.revertChanges=function(){
-            $scope.fieldEntity=angular.copy(me.content.fields);
-        };
-        me.registerEditChanges=function(){
-            $scope.rubedo.registerEditCtrl(me);
-        };
-        me.persistChanges=function(){
-            var payload=angular.copy(me.content);
-            payload.fields=angular.copy($scope.fieldEntity);
-            delete (payload.type);
-            RubedoContentsService.updateContent(payload).then(
-                function(response){
-                    $scope.rubedo.addNotification("success","Content updated.");
-                },
-                function(response){
-                    $scope.rubedo.addNotification("error","Content update error.");
-                }
-            );
-        };
-        $scope.registerFieldEditChanges=me.registerEditChanges;
-    }]);
 
     module.controller("CalendarController",["$scope","$route","RubedoContentsService","$element",function($scope,$route,RubedoContentsService, $element){
         var me = this;
