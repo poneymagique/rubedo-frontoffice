94a3273ef4feaddd1f21b1b2d8d5837f41f076de
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-10-07 14:33:49 +0200

    externalize content list

---
+++ src/modules/rubedoBlocks/controllers/ContentListController.js
@@ -0,0 +1,72 @@
+angular.module("rubedoBlocks").lazy.controller("ContentListController",['$scope','$compile','RubedoContentsService',function($scope,$compile,RubedoContentsService){
+    var me = this;
+    me.contentList=[];
+    var config=$scope.blockConfig;
+    var pageId=$scope.rubedo.current.page.id;
+    var siteId=$scope.rubedo.current.site.id;
+    me.contentHeight = config.summaryHeight?config.summaryHeight:80;
+    me.start = config.resultsSkip?config.resultsSkip:0;
+    me.limit = config.pageSize?config.pageSize:12;
+    var options = {
+        start: me.start,
+        limit: me.limit
+    };
+    if(config.singlePage){
+        options.detailPageId = config.singlePage;
+    }
+    me.titleOnly = config.showOnlyTitle;
+    me.columns = config.columns && !config.infiniteScroll ? 'col-md-'+(12/config.columns):'col-md-12';
+    me.showPaginator = config.showPager && !config.infiniteScroll;
+    me.changePageAction = function(){
+        options.start = me.start;
+        me.getContents(config.query, pageId, siteId, options);
+    };
+    if (config.infiniteScroll){
+        me.limit = options['limit'];
+        me.blockStyle = {
+            height: (me.limit * me.contentHeight - me.contentHeight)+'px',
+            'overflow-y': 'scroll'
+        };
+        me.timeThreshold = config['timeThreshold'] ? config['timeThreshold']:200;
+        me.scrollThreshold = config['scrollThreshold'] ? config['scrollThreshold']:300;
+    } else {
+        me.blockStyle = {
+            'overflow-y': 'visible'
+        };
+    }
+    me.getContents = function (queryId, pageId, siteId, options, add){
+        RubedoContentsService.getContents(queryId,pageId,siteId, options).then(function(response){
+            if (response.data.success){
+                me.count = response.data.count;
+                var columnContentList = [];
+                if (add){
+                    angular.forEach(response.data.contents,function(newContent){
+                        columnContentList.push(newContent);
+                    });
+                    me.contentList.push(columnContentList);
+                } else {
+                    me.contentList=[];
+                    angular.forEach(response.data.contents,function(newContent, key){
+                        columnContentList.push(newContent);
+                        if(config.columns && (key+1) % (Math.ceil(response.data.contents.length/config.columns)) == 0){
+                            me.contentList.push(columnContentList);
+                            columnContentList = [];
+                        }
+                    });
+                    if (columnContentList.length > 0){
+                        me.contentList.push(columnContentList);
+                    }
+                }
+            }
+        });
+    };
+    $scope.loadMoreContents = function(){
+        if (options['start'] + options['limit'] < me.count){
+            options['start'] += options['limit'];
+            me.getContents(config.query, pageId, siteId, options, true);
+        }
+    };
+    if(config.query){
+        me.getContents(config.query, pageId, siteId, options, false);
+    }
+}]);
