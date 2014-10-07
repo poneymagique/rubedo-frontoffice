86acfdff7fbdbc40957335567b8fe07507133711
Alexandru Dobre <alexandru.dobre@webtales.fr>
2014-10-07 13:17:43 +0200

    externalize carousel

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -59,7 +59,9 @@
             "template": "/templates/blocks/customTemplate.html"
         },
         "carrousel": {
-            "template": "/templates/blocks/carousel.html"
+            "template": "/templates/blocks/carousel.html",
+            "internalDependencies":["/src/modules/rubedoBlocks/controllers/CarouselController.js"],
+            "externalDependencies":['/components/OwlFonk/OwlCarousel/owl-carousel/owl.carousel.min.js']
         },
         "imageGallery": {
             "template": "/templates/blocks/gallery.html"
@@ -167,6 +169,13 @@
             var dependenciesArray=[ ];
             angular.forEach(bTypeArray,function(bType){
                 if (blocksConfig[bType]){
+                    if (blocksConfig[bType].externalDependencies){
+                        angular.forEach(blocksConfig[bType].externalDependencies,function(dependency){
+                            if (dependenciesArray.indexOf(dependency)<0){
+                                dependenciesArray.push(dependency);
+                            }
+                        });
+                    }
                     if (blocksConfig[bType].internalDependencies){
                         angular.forEach(blocksConfig[bType].internalDependencies,function(dependency){
                             var dependencyPath=themePath+dependency;
@@ -376,54 +385,7 @@
         }
     }]);
 
-    module.controller("CarouselController",["$scope","RubedoContentsService",function($scope,RubedoContentsService){
-        var me=this;
-        me.contents=[];
-        var blockConfig=$scope.blockConfig;
-        var queryOptions={
-            start: blockConfig.resultsSkip ? blockConfig.resultsSkip : 0,
-            limit: blockConfig.pageSize ? blockConfig.pageSize : 6,
-            'fields[]' : ["text","summary",blockConfig.imageField],
-            'requiredFields[]':[blockConfig.imageField]
-        };
-        var pageId=$scope.rubedo.current.page.id;
-        var siteId=$scope.rubedo.current.site.id;
-        me.getContents=function(){
-            RubedoContentsService.getContents(blockConfig.query,pageId,siteId, queryOptions).then(
-                function(response){
-                    if (response.data.success){
-                        me.contents=response.data.contents;
-                        setTimeout(function(){me.initCarousel();},100);
-                    }
-                }
-            );
-        };
-        me.initCarousel=function(){
-            var targetElSelector="#block"+$scope.block.id;
-            var owlOptions={
-                responsiveBaseWidth:targetElSelector,
-                singleItem:true,
-                pagination: blockConfig.showPager,
-                navigation: blockConfig.showNavigation,
-                autoPlay: blockConfig.autoPlay,
-                stopOnHover: blockConfig.stopOnHover,
-                paginationNumbers:blockConfig.showPagingNumbers,
-                navigationText: ['<span class="glyphicon glyphicon-chevron-left"></span>','<span class="glyphicon glyphicon-chevron-right"></span>'],
-                lazyLoad:true
-            };
-            angular.element(targetElSelector).owlCarousel(owlOptions);
-        };
-        me.getImageOptions=function(){
-            return({
-                height:blockConfig.imageHeight,
-                width:blockConfig.imageWidth ? blockConfig.imageWidth : angular.element("#block"+$scope.block.id).width(),
-                mode:blockConfig.imageResizeMode
-            });
-        };
-        if (blockConfig.query){
-            me.getContents();
-        }
-    }]);
+
 
 
     module.controller("AuthenticationController",["$scope","RubedoAuthService","snapRemote","RubedoPagesService",function($scope,RubedoAuthService,snapRemote,RubedoPagesService){
