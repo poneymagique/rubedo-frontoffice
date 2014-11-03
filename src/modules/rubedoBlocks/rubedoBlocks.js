704e8f06a403b80e0ecb8fbd9505733513bec137
Robin Lebert <robin.lebert@webtales.fr>
2014-11-03 18:17:01 +0100

    fix myme + scrollTo directive

--- src/modules/rubedoBlocks/rubedoBlocks.js
+++ src/modules/rubedoBlocks/rubedoBlocks.js
@@ -303,4 +303,18 @@
             };
         });
 
+    module.directive('scrollTo', function ($location, $anchorScroll) {
+        return function(scope, element, attrs) {
+            element.bind('click', function(event) {
+                event.stopPropagation();
+                scope.$on('$locationChangeStart', function(ev) {
+                    ev.preventDefault();
+                });
+                var location = attrs.scrollTo;
+                $location.hash(location);
+                $anchorScroll();
+            });
+        }
+    });
+
 })();
