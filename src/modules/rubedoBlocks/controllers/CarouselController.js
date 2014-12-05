67bd31b7776c20d43aad3fcbd367d883e1e7e438
goncalvesm <goncalves.mickael.60@gmail.com>
2014-12-05 09:57:33 +0100

    Fix propagation of the magic query status

--- src/modules/rubedoBlocks/controllers/CarouselController.js
+++ src/modules/rubedoBlocks/controllers/CarouselController.js
@@ -6,7 +6,8 @@ angular.module("rubedoBlocks").lazy.controller("CarouselController",["$scope","R
         start: blockConfig.resultsSkip ? blockConfig.resultsSkip : 0,
         limit: blockConfig.pageSize ? blockConfig.pageSize : 6,
         'fields[]' : ["text","summary",blockConfig.imageField],
-        'requiredFields[]':[blockConfig.imageField]
+        'requiredFields[]':[blockConfig.imageField],
+        ismagic: blockConfig.magicQuery ? blockConfig.magicQuery : false
     };
     var pageId=$scope.rubedo.current.page.id;
     var siteId=$scope.rubedo.current.site.id;
