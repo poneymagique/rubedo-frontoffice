c5173f46ef8b4ca20638e51f6144b07fe014c0c4
goncalvesm <goncalves.mickael.60@gmail.com>
2014-12-10 15:32:44 +0100

    #889 Fix content detail redirection

--- src/modules/rubedoBlocks/controllers/LanguageMenuController.js
+++ src/modules/rubedoBlocks/controllers/LanguageMenuController.js
@@ -1,7 +1,9 @@
-angular.module("rubedoBlocks").lazy.controller("LanguageMenuController", ['$scope', 'RubedoPagesService','RubedoModuleConfigService', '$route', '$location',
-    function ($scope, RubedoPagesService,RubedoModuleConfigService, $route, $location) {
+angular.module("rubedoBlocks").lazy.controller("LanguageMenuController", ['$scope', 'RubedoPagesService','RubedoModuleConfigService', 'RubedoContentsService', '$route', '$location',
+    function ($scope, RubedoPagesService,RubedoModuleConfigService, RubedoContentsService, $route, $location) {
         var me = this;
         var config = $scope.blockConfig;
+        var urlArray = [];
+        var contentId = "";
         me.languages = $scope.rubedo.current.site.languages;
         me.currentLang = $scope.rubedo.current.site.languages[$route.current.params.lang];
         me.mode = config.displayAs == "select";
@@ -20,7 +22,23 @@ angular.module("rubedoBlocks").lazy.controller("LanguageMenuController", ['$scop
                 RubedoModuleConfigService.changeLang(lang);
                 RubedoPagesService.getPageById($scope.rubedo.current.page.id).then(function(response){
                     if (response.data.success){
-                        $location.path(response.data.url);
+                        if($scope.rubedo.current.page.contentCanonicalUrl) {
+                            // Get content id
+                            urlArray = $scope.rubedo.current.page.contentCanonicalUrl.split("/");
+                            contentId = urlArray[urlArray.length-2];
+
+                            // Redirect without title
+                            $location.url(response.data.url + "/" + contentId + "/");
+
+                            //Redirect with title
+                            //RubedoContentsService.getContentById(contentId).then(function(contentResponse){
+                            //    if (contentResponse.data.success){
+                            //        $location.url(response.data.url + "/" + contentId + "/" + angular.lowercase(contentResponse.data.content.text.replace(/ /g, "-")));
+                            //    }
+                            //});
+                        } else {
+                            $location.url(response.data.url);
+                        }
                     }
                 });
             }
