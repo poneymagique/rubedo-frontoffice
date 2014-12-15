02095f77a670d16d554951df08208ab0744ffcab
goncalvesm <goncalves.mickael.60@gmail.com>
2014-12-15 09:26:16 +0100

    Allow to display more than 3 levels in the site map block

--- src/modules/rubedoBlocks/controllers/SiteMapController.js
+++ src/modules/rubedoBlocks/controllers/SiteMapController.js
@@ -1,31 +1,60 @@
 angular.module("rubedoBlocks").lazy.controller("SiteMapController",['$scope','$location','RubedoMenuService',function($scope,$location,RubedoMenuService){
     var me=this;
     var config=$scope.blockConfig;
-    if(config.rootPage){
-        me.menu={};
-        me.currentRouteline=$location.path();
-        var pageId=$scope.rubedo.current.page.id;
-        me.hidePages = true;
-        me.hideChildPages = true;
-        me.showPagesClick = function(pageType){
-            if(pageType == 'pages'){
-                me.hidePages = !me.hidePages;
-            } else {
-                me.hideChildPages = !me.hideChildPages;
-            }
-        };
-        if(config.displayLevel == 2){
-            me.hidePages = false;
-        } else if (config.displayLevel > 2){
-            me.hidePages = false;
-            me.hideChildPages = false;
-        }
-        RubedoMenuService.getMenu(config.rootPage, 5).then(function(response){
-            if (response.data.success){
-                me.menu=response.data.menu;
-            } else {
-                me.menu={};
+    var pageId=$scope.rubedo.current.page.id;
+    var hiddenPages = {};
+
+    var calculatePagesDiplay = function(pages, level) {
+        angular.forEach(pages, function(value) {
+            if(value.pages) {
+                if(level < config.displayLevel) {
+                    hiddenPages[value.id] = false;
+                } else {
+                    hiddenPages[value.id] = true;
+                }
+
+                calculatePagesDiplay(value.pages, level+1);
             }
         });
+    };
+
+    if(config.rootPage){
+        $scope.menu=[];
+        me.currentRouteline=$location.path();
+
+        // get all pages for the menu
+        RubedoMenuService.getMenu(config.rootPage, 5).then(function(response){
+            if (response.data.success){
+                $scope.menu=response.data.menu;
+
+                if($scope.menu.pages) {
+                    // hide home page children's if the display level is 0
+                    if(config.displayLevel == 0) {
+                        hiddenPages[$scope.menu.id] = true;
+                    } else {
+                        hiddenPages[$scope.menu.id] = false;
+                    }
+
+                    // calculate for all pages with children if they have to display them
+                    calculatePagesDiplay($scope.menu.pages, 1);
+                }
+            } else {
+                $scope.menu=[];
+            }
+        });
+
+        // allow to change the "show" status of a page
+        me.setPageDisplay = function(pageId){
+            if(hiddenPages[pageId] == true) {
+                hiddenPages[pageId] = false;
+            } else {
+                hiddenPages[pageId] = true;
+            }
+        };
+
+        // allow to get the "show" status of a page
+        me.getPageDisplay = function(pageId){
+            return  hiddenPages[pageId];
+        }
     }
 }]);
