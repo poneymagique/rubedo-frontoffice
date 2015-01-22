811b5f913a6ed784913f1996c50f9883795a07ba
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-01-22 10:52:24 +0100

    checkout user type processing

--- src/modules/rubedoBlocks/controllers/CheckoutController.js
+++ src/modules/rubedoBlocks/controllers/CheckoutController.js
@@ -1,4 +1,4 @@
-angular.module("rubedoBlocks").lazy.controller("CheckoutController",["$scope","RubedoPagesService","$rootScope","RubedoShoppingCartService", function($scope,RubedoPagesService,$rootScope,RubedoShoppingCartService){
+angular.module("rubedoBlocks").lazy.controller("CheckoutController",["$scope","RubedoPagesService","$rootScope","RubedoShoppingCartService","RubedoUserTypesService", function($scope,RubedoPagesService,$rootScope,RubedoShoppingCartService,RubedoUserTypesService){
     var me = this;
     var config = $scope.blockConfig;
     if (config.signupContentId){
@@ -13,7 +13,7 @@ angular.module("rubedoBlocks").lazy.controller("CheckoutController",["$scope","R
         });
     }
     me.blockId=($scope.block.id);
-    me.cartIsEmpty=true;
+    me.cartIsEmpty=false;
     me.detailedCart={};
     me.getCart=function(){
         RubedoShoppingCartService.getCart({includeDetail:true}).then(
@@ -22,7 +22,7 @@ angular.module("rubedoBlocks").lazy.controller("CheckoutController",["$scope","R
                     if (response.data.shoppingCart.detailedCart&&response.data.shoppingCart.detailedCart.totalItems>0){
                         me.detailedCart=response.data.shoppingCart.detailedCart;
                         me.cartIsEmpty=false;
-                        me.setCurrentStage(1);
+                        me.initializeCheckout();
                     } else  {
                         me.cartIsEmpty=true;
                         me.detailedCart={};
@@ -43,8 +43,69 @@ angular.module("rubedoBlocks").lazy.controller("CheckoutController",["$scope","R
         return parseInt(me.currentStage/me.maxStages*100);
     };
     me.setCurrentStage=function(newStage){
-        me.currentStage=newStage;
-        //angular.element("#checkoutStage"+newStage).collapse({parent:"#checkoutAccordion"});
+        if (newStage!=me.currentStage){
+            angular.element("#checkoutStage"+me.currentStage).collapse("hide");
+            angular.element("#checkoutStage"+newStage).collapse("show");
+            me.currentStage=newStage;
+        }
     };
+    me.parseUserType=function(userType){
+        me.userType=userType;
+        $scope.fieldIdPrefix="signUp"+"_"+me.userType.type;
+        me.userType.fields.unshift({
+            cType:"textfield",
+            config:{
+                name:"confirmPassword",
+                fieldLabel:$scope.rubedo.translate("Blocks.SignUp.label.confirmPassword"),
+                allowBlank:false,
+                vtype:"password"
+            }
+        });
+        me.userType.fields.unshift({
+            cType:"textfield",
+            config:{
+                name:"password",
+                fieldLabel:$scope.rubedo.translate("Blocks.SignUp.label.password"),
+                allowBlank:false,
+                vtype:"password"
+            }
+        });
+        me.userType.fields.unshift({
+            cType:"textfield",
+            config:{
+                name:"email",
+                fieldLabel:$scope.rubedo.translate("Label.Email"),
+                allowBlank:false,
+                vtype:"email"
+            }
+        });
+        me.userType.fields.unshift({
+            cType:"textfield",
+            config:{
+                name:"name",
+                fieldLabel:$scope.rubedo.translate("Blocks.SignUp.label.name"),
+                allowBlank:false
+            }
+        });
+        me.inputFields=me.userType.fields;
+    };
+    me.initializeCheckout=function(){
+        $scope.fieldIdPrefix="checkout";
+        $scope.fieldEntity={ };
+        $scope.fieldInputMode=true;
+        me.stage2Error=null;
+        if (!$scope.rubedo.current.user){
+            me.setCurrentStage(1);
+            if (config.userType){
+                RubedoUserTypesService.getUserTypeById(config.userType).then(
+                    function(response){
+                        if (response.data.success){
+                            me.parseUserType(response.data.userType);
+                        }
+                    }
+                );
+            }
+        }
+    }
 
 }]);
