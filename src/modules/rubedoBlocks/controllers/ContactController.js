58e48673a4b95c687b66691374c37a914adb725f
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-04-15 16:27:29 +0200

    multi ml contact

--- src/modules/rubedoBlocks/controllers/ContactController.js
+++ src/modules/rubedoBlocks/controllers/ContactController.js
@@ -1,16 +1,40 @@
-angular.module("rubedoBlocks").lazy.controller('ContactController',['$scope','RubedoContactService',function($scope,RubedoContactService){
+angular.module("rubedoBlocks").lazy.controller('ContactController',['$scope','RubedoContactService','RubedoMailingListService',function($scope,RubedoContactService,RubedoMailingListService){
     var me = this;
     var config = $scope.blockConfig;
     me.contactData={ };
     me.contactError=null;
     if (config.mailingListId){
         me.showForm=true;
+        if (angular.isArray(config.mailingListId)){
+            me.mailingListId=config.mailingListId[0];
+            if (config.mailingListId.length>1){
+                me.mailingLists=[];
+                RubedoMailingListService.getAllMailingList().then(function(response){
+                    if(response.data.success){
+                        angular.forEach(config.mailingListId, function(mailing){
+                            var newMailing = {};
+                            angular.forEach(response.data.mailinglists, function(mailingInfo){
+                                if(mailingInfo.id == mailing){
+                                    newMailing.id = mailing;
+                                    newMailing.name = mailingInfo.name;
+                                    me.mailingLists.push(newMailing);
+                                }
+                            });
+                        });
+                    }
+                });
+
+            }
+        } else {
+            me.mailingListId=config.mailingListId;
+        }
     }
+
     me.submit=function(){
         me.contactError=null;
         var contactSnap=angular.copy(me.contactData);
         var payload={
-            mailingListId:config.mailingListId,
+            mailingListId:me.mailingListId,
             from:contactSnap.email,
             subject:contactSnap.subject
         };
