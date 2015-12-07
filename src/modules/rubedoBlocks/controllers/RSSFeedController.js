41552778482719cce3d65b65043f1069c916a800
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-12-07 13:00:23 +0100

    add rss date

--- src/modules/rubedoBlocks/controllers/RSSFeedController.js
+++ src/modules/rubedoBlocks/controllers/RSSFeedController.js
@@ -6,6 +6,11 @@ angular.module("rubedoBlocks").lazy.controller("RSSFeedController",['$scope','$h
         $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num='+items+'&callback=JSON_CALLBACK&q=' + encodeURIComponent(config.feedUrl)).then(
             function(response){
                 me.feed=response.data.responseData.feed;
+                angular.forEach(me.feed.entries,function(entry){
+                    if (entry.publishedDate){
+                        entry.publishedDate=new Date(entry.publishedDate);
+                    }
+                });
             }
         );
     }
